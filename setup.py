06243ec573e5a34b9eb94fbbb7d92cd9524433f7
xnoproject <xno.cloud@gmail.com>
2025-08-22 15:38:40 +0700

    Update document

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="xnoapi",
-    version="0.1.16",
+    version="0.1.19",
     packages=find_packages(),
     install_requires=[
         "requests",
