415a834b62cc9962c02672e70eba7ca1c976fb57
xnoproject <xnocloud@gmail.com>
2025-09-19 13:05:46 +0700

    feat: update read the docs

--- source/conf.py
+++ source/conf.py
@@ -47,3 +47,26 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+
+
+# -- Options for LaTeX/PDF output --------------------------------------------
+# https://www.sphinx-doc.org/en/master/latex.html
+
+# Dùng xelatex để hỗ trợ Unicode (tiếng Việt)
+latex_engine = "xelatex"
+
+latex_elements = {
+    "babel": r"\usepackage[vietnamese]{babel}",  # giữ nguyên babel
+    "preamble": r"""
+\usepackage{fontspec}
+
+% Font chính
+\setmainfont{Times New Roman}
+\setsansfont{Arial}
+\setmonofont{Courier New}
+
+% Font emoji (Windows: Segoe UI Emoji, Linux: Noto Color Emoji, macOS: Apple Color Emoji)
+\newfontfamily\emoji{Segoe UI Emoji}
+\newcommand{\emoj}[1]{{\emoji #1}}
+""",
+}
