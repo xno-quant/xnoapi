a8d0ab21a0c8d7f52b9dd01321592fa53eb9cc44
xnoproject <xnocloud@gmail.com>
2025-09-19 15:08:34 +0700

    feat: update read the docs

--- source/conf.py
+++ source/conf.py
@@ -56,17 +56,11 @@ sys.path.insert(0, os.path.abspath(".."))
 latex_engine = "xelatex"
 
 latex_elements = {
-    "babel": r"\usepackage[vietnamese]{babel}",  # giữ nguyên babel
+    "babel": r"\usepackage[vietnamese]{babel}",  # ép dùng babel
     "preamble": r"""
 \\usepackage{fontspec}
-
-% Font chính
 \\setmainfont{Times New Roman}
 \\setsansfont{Arial}
 \\setmonofont{Courier New}
-
-% Font emoji (Windows: Segoe UI Emoji, Linux: Noto Color Emoji, macOS: Apple Color Emoji)
-\\newfontfamily\\emoji{Segoe UI Emoji}
-\\newcommand{\\emoj}[1]{{\\emoji #1}}
 """,
 }
